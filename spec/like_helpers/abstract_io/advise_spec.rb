8b9c62017835ea7232a176cae8cffd8b863a4e27
Jeremy Bopp <jeremy@bopp.net>
2024-12-26 21:09:12 -0600

    Fix a typo in a test name

--- spec/like_helpers/abstract_io/advise_spec.rb
+++ spec/like_helpers/abstract_io/advise_spec.rb
@@ -2,7 +2,7 @@
 require_relative '../../../spec_helper'
 
 describe "IO::LikeHelpers::AbstractIO#advise" do
-  it "raises NotImplementedError" do
+  it "returns nil" do
     io = IO::LikeHelpers::AbstractIO.new
     io.advise(:foo, 0, 1).should be_nil
   end
